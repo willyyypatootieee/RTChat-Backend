50c413b345c696504e3f2d3467fcbbbef918b9d9
willyyypatootieee <blueplayz26@gmail.com>
2026-07-15 00:43:41 +0700

    feat: refactor User entity and add UserDocument schema for MongoDB integration

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,17 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { AbstractEntity } from '../../common/database/abstract.entity';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({ versionKey: false })
 
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class User extends AbstractEntity {
+    @Prop()
+    @Field()
+    email!: string;
+
+    @Prop()
+    password!: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ src/users/entities/user.schema.ts
@@ -0,0 +1,13 @@
+import { AbstractSchema } from '../../common/database/abstract.entity';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({ versionKey: false })
+export class UserDocument extends AbstractSchema {
+    @Prop()
+    email!: string;
+
+    @Prop()
+    password!: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(UserDocument);
