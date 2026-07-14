43f7894a0abf53e307ddff63376a4ac1a14b24af
willyyypatootieee <blueplayz26@gmail.com>
2026-07-15 00:43:35 +0700

    feat: remove AbstractSchema definition from database module

--- src/common/database/abstract.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from '@nestjs/mongoose';
-import { SchemaTypes, Types } from 'mongoose';
-import { Prop } from '@nestjs/mongoose';
-
-@Schema()
-export class AbstractSchema {
-  @Prop({ type: SchemaTypes.ObjectId })
-  _id!: Types.ObjectId;
-}
