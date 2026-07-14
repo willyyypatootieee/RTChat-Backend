9fe21bd978ad16e59acae8181ffd91a4d6b65e65
willyyypatootieee <blueplayz26@gmail.com>
2026-07-14 22:11:14 +0700

    feat: bootstrap NestJS app with GraphQL and MongoDB configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,35 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import * as Joi from 'joi';
 import { DatabaseModule } from './common/database/database.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            validationSchema: Joi.object({
-                MONGODB_URI: Joi.string().required()
-            })
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      introspection: true,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault({
+          footer: false,
+          embed: true,
         }),
-        DatabaseModule,
-    ],
-    // controllers: [AppController],
-    // providers: [AppProviders],
+      ],
+    }),
+    DatabaseModule,
+    UsersModule,
+  ],
+  // controllers: [AppController],
+  // providers: [AppProviders],
 })
 export class AppModule {}
