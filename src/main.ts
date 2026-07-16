370c02392356102387282912d8a480b0a5fc9484
willyyypatootieee <blueplayz26@gmail.com>
2026-07-17 02:50:32 +0700

    feat: enable CORS and update application port to 3001

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors({ origin: '*' });
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
